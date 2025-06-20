0e105a8b3d0070afb4e4a8b106fa8751bc878e81
Matheow <95660245+DinoMatheow@users.noreply.github.com>
2025-06-19 22:18:53 -0500

    created of data svg

--- src/app/tech-profile/components/techCard/techCard.component.ts
+++ src/app/tech-profile/components/techCard/techCard.component.ts
@@ -1,9 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { TechCategory, TechItem } from '../../interfaces/tech.interface';
+import { TECH_DATA } from '../../../data/tech.data';
 
 @Component({
   selector: 'app-tech-card',
   imports: [],
   templateUrl: './techCard.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrl:'./techCard.component.css',
 })
-export class TechCardComponent { }
+export class TechCardComponent {
+
+  techData = signal(TECH_DATA);
+
+
+  selected = signal<TechItem | null>(null);
+  selectTech(item: TechItem) {
+    this.selected.set(item);
+  }
+
+}
